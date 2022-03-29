e96880963d3afc36a8b629b3d45b7aa6026a9d85
Danilo Kodavara <danilo.kodavara@b2wdigital.com>
2022-03-29 19:51:34 -0300

    feat: create structs of Player

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,18 +1,8 @@
 defmodule ExMon do
-  @moduledoc """
-  Documentation for `ExMon`.
-  """
+  alias ExMon.Player
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_player(name, mov_avg, move_rnd, move_heal) do
+    Player.build(name, mov_avg, move_rnd, move_heal)
   end
+
 end
